bae45a57649f188155d9d72e33a9a970e5d7dc62
Goutham Krishna K V <gauthamkrishna9991@live.com>
2019-05-12 23:02:36 +0530

    Added port requirements to Heroku App. Note that in normal development, change the port from '80' to '8080'.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-/*
+/*x
+    "node-first-journey" - Weather Application by Goutham Krishna
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
+    (at your option) any later version.
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
+    GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -21,15 +21,15 @@ const apiKey = '9969ca8563f38339014def2e4c57cbf0';
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended : true }))
+app.use(bodyParser.urlencoded({ extended : true }));
 
 app.get('/',(req,res)=>{
     res.render('index',{ weather: null, error: null });
-})
+});
 
 app.post('/',(req, res)=>{
     console.log(req.body.city);
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&units=metric&appid=${apiKey}`
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&units=metric&appid=${apiKey}`;
     request(url,(err, response, body)=>{
         if(err){
             res.render('index',{weather: null, error: 'Please Try Again'});
@@ -37,16 +37,18 @@ app.post('/',(req, res)=>{
         else{
             let weather = JSON.parse(body);
             // console.log(weather);
-            if(weather.main == undefined){
+            if(weather.main === undefined){
                 res.render('index', {weather: null, error: 'Please Try Again'});
             }
             else{
                 res.render('index',{weather: weather.main, error: null});
             }
-}
-        });
-// res.render('index',{ weather: null, error: null });
+        }
+    });
+    // res.render('index',{ weather: null, error: null });
+});
 
-})
+const port = process.env.PORT || 80;
 
-app.listen(3000);
+
+app.listen(port);
